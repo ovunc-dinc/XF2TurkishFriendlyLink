6e4fe8e2a1624d1820a1754d3ef2f2ffff249b15
Ovunc Dinc <34074321+ovuncdinc@users.noreply.github.com>
2017-11-29 01:13:56 +0300

    Create Listener.php

---
+++ Listener.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Dinco\TurkishFriendlyLink;
+
+class Listener
+{
+    /**
+     * @param \XF\Container $container
+     * @param \XF\Mvc\Router $router
+     * @see \XF\Mvc\Router::prepareStringForUrl()
+     * @see utf8_deaccent
+     * @see utf8_romanize
+     */
+    public static function routerPublicSetup(\XF\Container $container, \XF\Mvc\Router &$router)
+    {
+        global $UTF8_LOWER_ACCENTS, $UTF8_UPPER_ACCENTS, $UTF8_ROMANIZATION;
+
+        $mapLower = [
+            'o' => ['ö'],
+            'c' => ['ç'],
+            'g' => ['ğ'],
+            's' => ['ş'],
+            'u' => ['ü'],
+            'a' => ['â'],
+            'e' => ['ê'],
+			'i' => ['ı', 'î', 'é'],
+            '' => ['̉', '̣', '̃', '̀', '́'],
+        ];
+        foreach ($mapLower as $to => $fromMany) {
+            foreach ($fromMany as $from) {
+                $UTF8_LOWER_ACCENTS[$from] = $to;
+            }
+        }
+
+        $mapUpper = [
+            'O' => ['Ö'],
+            'C' => ['Ç'],
+            'G' => ['Ğ'],
+            'S' => ['Ş'],
+            'U' => ['Ü'],
+            'A' => ['Â'],
+            'E' => ['Ê'],
+			'İ' => ['I', 'Î', 'Ê'],
+            '' => ['̉', '̣', '̃', '̀', '́'],
+        ];
+        foreach ($mapUpper as $to => $fromMany) {
+            foreach ($fromMany as $from) {
+                $UTF8_UPPER_ACCENTS[$from] = $to;
+            }
+        }
+
+        $UTF8_ROMANIZATION["🇻🇳"] = 'vn';
+
+        $router->setRomanizeUrls(true);
+    }
+}
